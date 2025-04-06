fb79fe07b7843e20ad2b3b0a5f26e0e28406568f
otaviolimasantos09 <otaviolimasantos09@gmail.com>
2025-04-06 22:38:31 +0000

    criaçao do javascript da funçao para receber o valor da conta e da funçao para receber o numero de pessoas

---
+++ main.js
@@ -0,0 +1,13 @@
+let bill = 0
+let tipPercentage = 0
+let numberOfPeople = 0
+
+        function receiveBillValue(){
+            bill = document.querySelector('#bill').valueAsNumber
+            console.log(bill)
+        }
+
+        function recieveNumberOfPeopleValue() {
+            numberOfPeople = document.querySelector('#people').valueAsNumber
+            
+        }
